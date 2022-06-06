29c41907c7e6357096996e2d67a685b94a101427
genildocs <genildocs@gmail.com>
2022-06-06 02:27:56 -0300

    TransformaTec_Tarde ReactJS

---
+++ src/components/aula-53/Hello.js
@@ -0,0 +1,20 @@
+
+
+function Hello(){
+
+    return(
+
+        <>
+          <h1>
+              Hello Word!
+          </h1>
+          <p>
+              Fugindo da maldição do Hello Word. :)
+          </p>
+        </>
+
+    )
+
+}
+
+export default Hello
